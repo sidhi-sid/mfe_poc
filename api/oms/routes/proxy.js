73b4aa242288709f8b20adef3c2dfb3f3af86866
Sanchit Tandon <sanchittandon@VF413.local>
2026-03-11 12:37:05 +0530

    removed API directory

--- api/oms/routes/proxy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const proxyController = require('../controllers/proxyController');
-
-async function proxyRoutes(fastify) {
-  fastify.all('/*', proxyController.proxyToLoopbackHandler);
-}
-
-module.exports = proxyRoutes;
